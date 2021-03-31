dd005a794fd4e8ef49bd7c1af3df2562e45005bd
Kobi Karadi <karadikobi1@gmail.com>
2021-03-31 20:10:14 +0300

    j

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const config = require("config");
-const Joi = require("joi");
+const Joi = require("jowi");
 
 function auth(req, res, next) {
   const token = req.header("x-auth-token");
